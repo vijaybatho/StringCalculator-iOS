4c98f32291cf64339d20c686afe52fae4b19000e
Vijay Batho <76720991+vijaybatho@users.noreply.github.com>
2025-06-05 08:14:30 +0530

    Initial Xcode project setup

---
+++ StringCalculator/StringCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StringCalculator
+//
+//  Created by Vijay Batho on 05/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StringCalculator/StringCalculatorTests/StringCalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  StringCalculatorTests.swift
+//  StringCalculatorTests
+//
+//  Created by Vijay Batho on 05/06/25.
+//
+
+import Testing
+@testable import StringCalculator
+
+struct StringCalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
